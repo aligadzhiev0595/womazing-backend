ef5386cda525758fa61e2a3d4ec2e2358d566acb
aligadzhiev0595 <aligadzhiev0595@gmail.com>
2022-11-21 02:22:58 +0300

    feature: create server & data base

---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import express, { Express } from 'express'
+import { connect } from 'mongoose'
+import cors from 'cors'
+
+const PORT = process.env.PORT || 8080
+const app: Express = express()
+
+app.use(cors())
+const jsonBodyMiddleware = express.json()
+app.use(jsonBodyMiddleware)
+app.use('/', require('./routes/main.routes'))
+
+
+async function start() {
+  try {
+    await connect(
+      'mongodb+srv://root:root@cluster0.na6fqa7.mongodb.net/shop?retryWrites=true&w=majority'
+    ).then(() => console.log(`⚡️[dataBase]: DataBase is running`))
+    app.listen(PORT, () => {
+      console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`)
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+start()
