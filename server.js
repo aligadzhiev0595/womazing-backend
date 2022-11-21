fbc4eaa72282b0227ccd24a8acde3e28b6fe68f8
aligadzhiev0595 <aligadzhiev0595@gmail.com>
2022-11-21 03:37:38 +0300

    test-deploy: ---------------try

--- server.js
+++ server.js
@@ -2,19 +2,22 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 7070
 const app = express()
 
 app.use(cors())
 app.use(express.json({ extended: true }))
-app.use('/', require('./routes/main.routes'))
 app.use('/api/products', require('./routes/products.routes'))
 
 async function start() {
   try {
     await mongoose.connect(
-      'mongodb+srv://root:root@cluster0.na6fqa7.mongodb.net/shop?retryWrites=true&w=majority'
-    ).then(() => console.log(`⚡️[dataBase]: DataBase is running`)).catch((e) => console.log(`⚡️[error]: DataBase ${e}`))
+      'mongodb+srv://root:root@cluster0.na6fqa7.mongodb.net/shop?retryWrites=true&w=majority',
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    )
     app.listen(PORT, () => {
       console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`)
     })
