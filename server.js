0910bad25b22eaf657cb881b379bccdb29229390
aligadzhiev0595 <aligadzhiev0595@gmail.com>
2022-11-21 03:29:10 +0300

    test-deploy: returned the previous settings & change express json parser func

--- server.js
+++ server.js
@@ -6,8 +6,7 @@ const PORT = process.env.PORT || 8080
 const app = express()
 
 app.use(cors())
-const jsonBodyMiddleware = express.json()
-app.use(jsonBodyMiddleware)
+app.use(express.json({ extended: true }))
 app.use('/', require('./routes/main.routes'))
 app.use('/api/products', require('./routes/products.routes'))
 
@@ -15,7 +14,7 @@ async function start() {
   try {
     await mongoose.connect(
       'mongodb+srv://root:root@cluster0.na6fqa7.mongodb.net/shop?retryWrites=true&w=majority'
-    )
+    ).then(() => console.log(`⚡️[dataBase]: DataBase is running`)).catch((e) => console.log(`⚡️[error]: DataBase ${e}`))
     app.listen(PORT, () => {
       console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`)
     })
